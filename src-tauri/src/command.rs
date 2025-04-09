0775e3693f0509c319748c1a569b02c8d86263fd
maidang1 <497350746@qq.com>
2025-04-10 01:33:17 +0800

    feat: add ollama single

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,21 +1,19 @@
 use ollama_rs::Ollama;
+use once_cell::sync::Lazy;
 
-
-
+static OLLAMA: Lazy<Ollama> = Lazy::new(|| Ollama::default());
 
 #[tauri::command]
 pub fn my_custom_command() {
-  println!("I was invoked from JavaScript!");
+    println!("I was invoked from JavaScript!");
 }
 
 #[tauri::command]
 pub async fn list_modules() -> String {
-  let ollama = Ollama::default();
-  let modules = ollama.list_local_models().await.unwrap();
-  modules
-    .iter()
-    .map(|m| m.name.clone())
-    .collect::<Vec<String>>()
-    .join(", ")
-
+    let modules = OLLAMA.list_local_models().await.unwrap();
+    modules
+        .iter()
+        .map(|m| m.name.clone())
+        .collect::<Vec<String>>()
+        .join(", ")
 }
