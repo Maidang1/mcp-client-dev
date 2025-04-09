53ed1cccf4cded9c930f48c6f69ee9c8220f724e
maidang1 <497350746@qq.com>
2025-04-10 01:05:12 +0800

    feat: add sender

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,29 @@
 import { useState } from "react";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/core";
+// import logo from "./assets/logo.svg";
+import { Sender } from '@ant-design/x'
+// import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+  // const [name, setName] = useState("");
 
   async function greet(e: React.FormEvent) {
-    e.preventDefault();
-    setGreetMsg(await invoke("greet", { name }));
+    // e.preventDefault();
+    // setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <Sender
+        value={value}
+        onChange={(v) => setValue(v)}
+        onSubmit={() => {
+          alert(value);
+        }}
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank" rel="noopener">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank" rel="noopener">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={greet}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      />
+      <div>{value}</div>
     </main>
   );
 }
